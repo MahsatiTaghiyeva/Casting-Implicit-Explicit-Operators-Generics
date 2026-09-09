878fbbfe101e5d11875380578ad8ba088469f845
MahsatiTaghiyeva <mahsatist@code.edu.az>
2026-09-09 16:30:40 +0400

    added currency task

---
+++ Currency/Currency.cs
@@ -0,0 +1,6 @@
+public enum Currency
+{
+    Usd ,
+    Eur,
+    Try
+}
--- Currency/Program.cs
+++ Currency/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Exchange(Enum currency, double azn)
+{
+    if(currency is not Currency)
+        return 0;
+    if(currency is Currency.Eur)
+        return azn* 0.59;
+    if(currency is Currency.Try)
+        return azn*0.2;
+    if(currency is Currency.Usd)
+        return azn*0.7;
+    return 0;
+}
+Console.WriteLine(Exchange(Currency.Eur, 23));
